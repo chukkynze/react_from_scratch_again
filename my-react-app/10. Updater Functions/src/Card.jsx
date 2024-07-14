9bf51fedea6b38a2adea4db1324ca1a2a12e4367
Chukky Nze <chukkynze@gmail.com>
2024-07-13 21:33:19 -0700

    Updater functions: Queueing function calls

---
+++ my-react-app/10. Updater Functions/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-img" alt="Profile Picture" src="https://media.licdn.com/dms/image/C4E03AQEBoIyqAg5nFA/profile-displayphoto-shrink_800_800/0/1623943191441?e=1726099200&v=beta&t=AbC4v2zt2yhadgDd7q2dEL2WP2JU8oaA9b7prQP1IWY" />
+            <h2 className="card-title">Chukky Nze</h2>
+            <p className="card-text">I code & run startups. I also love sci-fi games</p>
+        </div>
+    );
+}
+
+export default Card
